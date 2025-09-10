d65d15b87eae2543b661f6ce77f2653f4617f98f
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-09-10 10:00:38 -0400

    initial commit

---
+++ src/lab_02/Lab_02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_02;
+
+/**
+ *
+ * @author 6298805
+ */
+public class Lab_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
